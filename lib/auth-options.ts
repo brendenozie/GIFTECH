c99d4d5cd9c066dbe6b6741cad950e5795252377
brendenozie <brendenozie@gmail.com>
2025-12-30 10:10:30 +0300

    Enhance JWT token generation to include user email for improved security

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -146,7 +146,7 @@ export const authOptions: NextAuthOptions = {
           token.image = dbUser.image;
           
           // Generate JWT token for our app
-          token.appToken = generateToken(dbUser._id.toString());
+          token.appToken = generateToken(dbUser._id.toString(), dbUser.email);
         }
       }
       return token;
